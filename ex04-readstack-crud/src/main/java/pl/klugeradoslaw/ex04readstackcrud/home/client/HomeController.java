b52086124b9b4372c20b33d61077e7113cd4437e
klugeradoslaw <kluge.radoslaw@gmail.com>
2023-03-08 11:11:37 +0100

    HomeController.class added to repo

--- ex04-readstack-crud/src/main/java/pl/klugeradoslaw/ex04readstackcrud/home/client/HomeController.java
+++ ex04-readstack-crud/src/main/java/pl/klugeradoslaw/ex04readstackcrud/home/client/HomeController.java
@@ -1,4 +1,25 @@
 package pl.klugeradoslaw.ex04readstackcrud.home.client;
 
-public class HomeController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import pl.klugeradoslaw.ex04readstackcrud.domain.api.DiscoveryBasicInfoDto;
+import pl.klugeradoslaw.ex04readstackcrud.domain.api.DiscoveryService;
+
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet
+public class HomeController extends HttpServlet {
+    private DiscoveryService discoveryService = new DiscoveryService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<DiscoveryBasicInfoDto> discoveries = discoveryService.findAll();
+        req.setAttribute("Discoveries", discoveries);
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
 }
