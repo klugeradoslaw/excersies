c94a37ca1c43763155261813ec6789f61933154c
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-03-13 14:52:08 +0100

    classes: BaseDao, CategoryDao, DiscoveryDao unified

--- ex04-readstack-crud/src/main/java/pl/klugeradoslaw/ex04readstackcrud/domain/discovery/DiscoveryDao.java
+++ ex04-readstack-crud/src/main/java/pl/klugeradoslaw/ex04readstackcrud/domain/discovery/DiscoveryDao.java
@@ -1,6 +1,7 @@
 package pl.klugeradoslaw.ex04readstackcrud.domain.discovery;
 
 import pl.klugeradoslaw.ex04readstackcrud.config.DataSourceProvider;
+import pl.klugeradoslaw.ex04readstackcrud.domain.common.BaseDao;
 
 import javax.naming.NamingException;
 import javax.sql.DataSource;
@@ -9,20 +10,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DiscoveryDao {
-    private final DataSource dataSource;
-
-
-    public DiscoveryDao() {
-
-        try {
-            this.dataSource = DataSourceProvider.getDataSource();
-        } catch (NamingException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
+public class DiscoveryDao extends BaseDao {
     public List<Discovery> findAll() {
         final String query = """
                 SELECT
@@ -31,7 +19,7 @@ public class DiscoveryDao {
                     discovery d
                 """;
 
-        try (Connection connection = dataSource.getConnection();
+        try (Connection connection = getConnection();
              Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(query);
             List<Discovery> allDiscoveries = new ArrayList<>();
@@ -55,7 +43,7 @@ public class DiscoveryDao {
                 WHERE
                     category_id = ?
                 """;
-        try (Connection connection = dataSource.getConnection();
+        try (Connection connection = getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setInt(1, categoryId);
             ResultSet resultSet = statement.executeQuery();
